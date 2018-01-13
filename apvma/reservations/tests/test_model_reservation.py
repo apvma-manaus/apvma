5c7e91bdcb12df85f7e0c630cf8659c98d523798
Bruno Luiz Santana de Araujo <santanasta@gmail.com>
2018-01-13 18:03:35 -0400

    CPF only numbers in the request_signup_mail

--- apvma/reservations/tests/test_model_reservation.py
+++ apvma/reservations/tests/test_model_reservation.py
@@ -10,7 +10,7 @@ class ReservationModelTest(TestCase):
         self.user = User.objects.create_user(username='bruno', password='1234')
         self.client.login(username='bruno', password='1234')
         self.reservation = Reservation.objects.create(
-            user=self.user, date='2018-01-10', spot='Tapiri'
+            user=self.user, date='2018-01-10', spot='TP'
         )
 
     def test_create(self):
@@ -19,4 +19,4 @@ class ReservationModelTest(TestCase):
     def test_nuiqueness(self):
         """One spot can not be reserved for two apartments at the same day"""
         with self.assertRaises(IntegrityError):
-            reservation_already_exists = Reservation.objects.create(date='2018-01-10', spot='Tapiri')
+            reservation_already_exists = Reservation.objects.create(date='2018-01-10', spot='TP')
